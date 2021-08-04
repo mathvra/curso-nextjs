1a3a5a8a0d6d127b0612c396ea9f0b365d98f499
mathvra <matheusvram@gmail.com>
2021-08-03 22:56:43 -0300

    sistema de modulos e integracao TS

---
+++ exercicios/components/modulo/funcionais.jsx
@@ -0,0 +1,21 @@
+export function Comp1(){
+    return (
+        <div>
+            <h2>Comp #01</h2>
+        </div>
+    )
+}
+
+export const Comp2 = function() {
+    return <h2>Comp #02</h2>
+}
+
+export default function Comp3(){
+    return (
+        <h2>Comp #03</h2>
+    )
+}
+
+export const Comp4 = () => {
+    return <h2>Comp #04</h2>
+}
---
+++ exercicios/pages/modulo/teste.jsx
@@ -0,0 +1,12 @@
+import Padrao, {Comp1 as Blabla, Comp2, Comp4} from "../../components/modulo/funcionais"
+
+export default function teste(){
+    return(
+        <div>
+            <Blabla/>
+            <Comp2 />
+            <Padrao />
+            <Comp4 />
+        </div>
+    )
+}
